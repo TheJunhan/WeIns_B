11dd0e1c4186fa6959d36ec8851d08268909ffb8
Laffery <2387065420@qq.com>
2020-07-07 09:20:58 +0800

    :seedling: first commit

---
+++ src/main/java/com/back/weins/WeinsApplication.java
@@ -0,0 +1,13 @@
+package com.back.weins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/weins/WeinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.weins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
