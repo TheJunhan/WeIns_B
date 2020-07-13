713280de5d15837ad43256dcdba399839ca008e7
Laffery <49607541+Laffery@users.noreply.github.com>
2020-07-13 11:37:41 +0800

    Update and rename userRepository.java to UserRepository.java
    fix: rename

--- src/main/java/com/back/weins/repository/UserRepository.java
+++ src/main/java/com/back/weins/repository/UserRepository.java
@@ -4,7 +4,7 @@ import com.back.weins.entity.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface userRepository extends JpaRepository<user, Integer> {
+public interface UserRepository extends JpaRepository<user, Integer> {
     @Query( value = "select ?1 not in ( select phone from user ) as res", nativeQuery=true)
     Integer check(String phone);
     @Query( value = "select * from user where user.phone = ?1", nativeQuery=true)
