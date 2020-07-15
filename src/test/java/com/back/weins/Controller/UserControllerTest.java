14a873aab067a196de1ed18dc568916ce94bdef3
Laffery <2387065420@qq.com>
2020-07-15 22:51:58 +0800

    :fire: fix: user controller test

--- src/test/java/com/back/weins/Controller/UserControllerTest.java
+++ src/test/java/com/back/weins/Controller/UserControllerTest.java
@@ -1,7 +1,59 @@
 package com.back.weins.Controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.back.weins.WeinsApplicationTests;
+import com.back.weins.servicesImpl.UserServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class UserControllerTest {
+import org.springframework.web.context.WebApplicationContext;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class UserControllerTest extends WeinsApplicationTests {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    UserServiceImpl userService;
+
+    @Autowired
+    UserController userController;
+
+    private ObjectMapper om = new ObjectMapper();
+
+    @BeforeEach
+    public void start() {
+        System.out.println("test start");
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @AfterEach
+    public void end() {
+        System.out.println("test end");
+    }
+
+    @Test
+    public void getOne() throws Exception {
+        MvcResult result = mockMvc.perform(get("/user/getOne?id=1")
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("徐珺涵"))
+                .andReturn();
+    }
 }
