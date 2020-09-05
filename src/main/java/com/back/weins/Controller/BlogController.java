20c1bff63c0ffba1148d3e94b3eab3018632aef3
Tarosweet <fuyuhan@sjtu.edu.cn>
2020-09-05 18:17:03 +0800

    :fire: finish log

--- src/main/java/com/back/weins/Controller/BlogController.java
+++ src/main/java/com/back/weins/Controller/BlogController.java
@@ -11,6 +11,9 @@ import com.back.weins.services.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 @RestController
@@ -24,33 +27,46 @@ public class BlogController {
 
     @GetMapping(value="/setLabel")
     public String setLabel(@RequestParam("label") String label) {
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        LOG.info(" set label: "+label);
         return blogService.setLabel(label);
     }
 
     @GetMapping(value="/getLabels")
     public List<Label> getLabels() {
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        LOG.info(" get labels ");
         return blogService.getLabels();
     }
 
     @GetMapping(value = "/findFuzzyLabels")
     public List<Label> findLabels(@RequestParam("lab") String lab) {
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        LOG.info(" get labels by fuzzyname: "+lab);
         return blogService.findLabels(lab);
     }
 
     @PostMapping("/setBlog")
     public Integer setBlog(@RequestBody BlogUtil blogUtil) {
         System.out.println(blogUtil);
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        LOG.info("user : "+blogUtil.getUid()+" set blog : type: "+blogUtil.getType()+" content : "+blogUtil.getContent()+" day : "+
+                blogUtil.getPost_day()+" video "+ blogUtil.getVideo()+" img "+ blogUtil.getImag()+" label "+ blogUtil.getLabel());
         return blogService.setBlog(blogUtil.getUid(), blogUtil.getType(), blogUtil.getContent(),
                 blogUtil.getPost_day(), blogUtil.getVideo(), blogUtil.getImag(), blogUtil.getLabel());
     }
 
     @GetMapping("/getPublicBlogs")
     public List<JSONObject> getPublicBlogs(){
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        LOG.info(" getPublicBlogs");
         return blogService.getPublicBlog();
     }
 
     @GetMapping("/page/getPublicBlogs")
     public List<JSONObject> getPublicBlogs_page(@RequestParam("index") Integer index, @RequestParam("num") Integer num) {
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        LOG.info(" getPublicBlogs by page : index:"+index+" num:"+ num);
         return blogService.getPublicBlog_page(index, num);
     }
 
@@ -59,16 +75,22 @@ public class BlogController {
                                                  @RequestParam("uid") Integer uid,
                                                  @RequestParam("index") Integer index,
                                                  @RequestParam("num") Integer num) {
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        LOG.info("user : "+uid+" getBlogs by label : labelid: "+lid+" page  index: "+index+" num: "+ num);
         return blogService.getBlogsByLabel_page(lid, uid, index, num);
     }
 
     @GetMapping("/getBlogsByLabel")
     public List<JSONObject> getBlogsByLabel(@RequestParam("lid") Integer lid, @RequestParam("uid") Integer uid){
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        LOG.info("user : "+uid+" getBlogs by label : labelid: "+lid);
         return blogService.getBlogsByLabel(lid, uid);
     }
 
     @GetMapping("/getBlogsLogined")
     public List<JSONObject> getBlogsLogined(@RequestParam("uid") Integer uid){
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        LOG.info("user : "+uid+" getBlogslogined ");
         return blogService.getBlogsLogined(uid);
     }
 
@@ -76,53 +98,111 @@ public class BlogController {
     public List<JSONObject> getBLogsLogined_page(@RequestParam("uid") Integer uid,
                                                  @RequestParam("index") Integer index,
                                                  @RequestParam("num") Integer num) {
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        LOG.info("user : "+uid+" getBlogslogined by page: "+" page  index: "+index+" num: "+ num);
         return blogService.getBlogsLogined_page(uid, index, num);
     }
 
     @GetMapping("/getBlogsById")
     public List<JSONObject> getBlogsById(@RequestParam("uid") Integer uid, @RequestParam("to_see_uid") Integer to_see_uid) {
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        LOG.info("user : "+uid+" getBlogs by id: "+to_see_uid);
         return blogService.getBlogsById(uid, to_see_uid);
     }
 
     @GetMapping("/like")
     public boolean setLike(@RequestParam("uid") Integer uid, @RequestParam("bid") Integer bid){
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        LOG.info("user : "+uid+" liked Blog -- blogid: "+bid);
         return blogService.setLike(uid, bid);
     }
 
     @GetMapping("/collect")
     public boolean setCollect(@RequestParam("uid") Integer uid, @RequestParam("bid") Integer bid, @RequestParam("flag") boolean flag){
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        if(flag==true)
+        {
+            LOG.info("user : "+uid+" collected Blog -- blogid: "+bid);
+        }
+        else
+        {
+            LOG.info("user : "+uid+" uncollected Blog -- blogid: "+bid);
+        }
         return blogService.setCollect(uid, bid, flag);
     }
 
     @GetMapping("/removeLike")
     public boolean removeLike(@RequestParam("uid") Integer uid, @RequestParam("bid") Integer bid){
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        LOG.info("user : "+uid+" unliked Blog -- blogid: "+bid);
         return blogService.removeLike(uid, bid);
     }
 
     @PostMapping("/setReblog")
     public boolean setReblog(@RequestBody ReblogUtil reblogUtil){
         System.out.print(reblogUtil);
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        LOG.info("user : "+reblogUtil.getUid()+" set reblog :"+reblogUtil.getBid()+" type: "+reblogUtil.getType()+" content : "+reblogUtil.getContent()+" day : "+
+                reblogUtil.getPost_day());
         return blogService.setReblog(reblogUtil.getUid(), reblogUtil.getBid(), reblogUtil.getType(), reblogUtil.getContent(), reblogUtil.getPost_day());
     }
 
     @GetMapping("/removeBlog")
     public boolean removeBlog(@RequestParam("uid") Integer uid, @RequestParam("bid") Integer bid, @RequestParam("type") Integer type){
-        return blogService.removeBlog(uid, bid, type);
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        boolean result=blogService.removeBlog(uid, bid, type);
+        if(result)
+        {
+            LOG.info("user : "+uid+" removed Blog successfully-- blogid: "+bid);
+        }
+        else
+        {
+            LOG.info("user : "+uid+" failed to remove Blog -- blogid: "+bid);
+        }
+
+        return result;
     }
 
     @PostMapping("/setComment")
     public boolean setComment(@RequestBody CommentUtils commentUtils){
-        return blogService.setComment(commentUtils.getUid(), commentUtils.getTo_uid(), commentUtils.getBid(), commentUtils.getContent(), commentUtils.getPost_time(), commentUtils.getTo_cid(), commentUtils.getRoot_cid());
+        boolean result=blogService.setComment(commentUtils.getUid(), commentUtils.getTo_uid(), commentUtils.getBid(), commentUtils.getContent(), commentUtils.getPost_time(), commentUtils.getTo_cid(), commentUtils.getRoot_cid());
+
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+
+        if(result)
+        {
+            LOG.info("user : "+commentUtils.getUid()+" commented  blog :"+commentUtils.getBid()+" of user :"+commentUtils.getTo_uid()+"  successfully, comment id :"+commentUtils.getTo_cid()+" root comment id :"+commentUtils.getRoot_cid());
+        }
+        else
+        {
+            LOG.info("user : "+commentUtils.getUid()+" commented  blog :"+commentUtils.getBid()+" of user :"+commentUtils.getTo_uid()+"  unsuccessfully, comment id :"+commentUtils.getTo_cid()+" root comment id :"+commentUtils.getRoot_cid());
+        }
+
+//        return blogService.setComment(commentUtils.getUid(), commentUtils.getTo_uid(), commentUtils.getBid(), commentUtils.getContent(), commentUtils.getPost_time(), commentUtils.getTo_cid(), commentUtils.getRoot_cid());
+        return result;
     }
 
     @PostMapping("/removeComment")
     public boolean removeComment(@RequestParam("uid") Integer uid, @RequestParam("cid") Integer cid, @RequestParam("type") Integer type){
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        LOG.info("user : "+uid+" remove comment -- commentid: "+cid+ " type "+type);
+//        LOG.info("user : "commentUtils.getUid()+" commented  blog :"+commentUtils.getBid()+" of user :"+commentUtils.getTo_uid()+" , comment id :"+commentUtils.getTo_cid()+" root comment id :"+commentUtils.getRoot_cid())
         return blogService.removeComment(uid, cid, type);
     }
 
     @PostMapping("/changeBlog")
     public boolean changeBlog(@RequestBody ChangeUtil changeUtil) {
-        return blogService.changeBlog(changeUtil.getUid(), changeUtil.getBid(), changeUtil.getContent(), changeUtil.getType());
+        Logger LOG = LoggerFactory.getLogger(BlogController.class);
+        boolean result=blogService.changeBlog(changeUtil.getUid(), changeUtil.getBid(), changeUtil.getContent(), changeUtil.getType());
+        if(result)
+        {
+            LOG.info("user : "+changeUtil.getUid()+"changed blog "+changeUtil.getBid()+" successfully,content "+changeUtil.getContent()+" type: "+changeUtil.getType());
+        }
+        else
+        {
+            LOG.info("user : "+changeUtil.getUid()+"changed blog "+changeUtil.getBid()+" unsuccessfully,content "+changeUtil.getContent()+" type: "+changeUtil.getType());
+        }
+        return result;
     }
 
     @GetMapping("/getSingleBlog")
--- src/main/java/com/back/weins/Controller/UserController.java
+++ src/main/java/com/back/weins/Controller/UserController.java
@@ -11,12 +11,17 @@ import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 import java.util.Objects;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+//    logger LOG = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     UserServiceImpl userService;
 
@@ -25,39 +30,79 @@ public class UserController {
 
     @GetMapping("/getOne")
     public User getOne(@RequestParam("id") Integer id) {
+        Logger LOG = LoggerFactory.getLogger(UserController.class);
+        LOG.info(" get user by id: "+id);
         return userService.getByID(id);
     }
 
     @GetMapping("/getByName")
     public User getByName(@RequestParam("name") String name) {
+        Logger LOG = LoggerFactory.getLogger(UserController.class);
+        LOG.info(" get user by name: "+name);
         return userService.getByName(name);
     }
 
     @GetMapping("/getByFuzzyName")
     public List<User> getByFuzzyName(@RequestParam("name") String name) {
+        Logger LOG = LoggerFactory.getLogger(UserController.class);
+        LOG.info(" get user by fuzzyname: "+name);
         return userService.getByFuzzyName(name);
     }
 
     @GetMapping("/getPlainOne")
     public User getPlainOne(@RequestParam("id") Integer id) {
         User user = userService.getByID(id);
+        Logger LOG = LoggerFactory.getLogger(UserController.class);
+        if (user == null) {
+            LOG.info(" the finding user: "+id+" does not exist");
+        }
+        else  {
+            LOG.info(" found user by id: "+id);
+        }
         user.setPassword(null);
         return user;
     }
 
     @GetMapping("/getAll")
     public List<User> getAll() {
+        Logger LOG = LoggerFactory.getLogger(UserController.class);
+        LOG.info("get all users");
         return userService.getAll();
     }
 
     @PostMapping("/register")
     public String register(@RequestBody RegisterUtil registerUtil) {
-        return userService.register(registerUtil);
+//        return userService.register(registerUtil);
+        Logger LOG = LoggerFactory.getLogger(UserController.class);
+        String result=userService.register(registerUtil);
+        if (result == "phone error") {
+            LOG.info("user: "+registerUtil.getPhone()+" register phone error");
+        }
+        else if (result == "name error") {
+            LOG.info("user: "+registerUtil.getName()+" register name error");
+        }
+        else{
+            LOG.info("user: "+registerUtil.getPhone()+" register successfully pwd: "+registerUtil.getPassword());
+        }
+
+        return result;
     }
 
     @PostMapping("/login")
     public User login(@RequestParam("ph") String phone, @RequestParam("pwd") String password){
-        return userService.login(phone, password);
+//        logger LOG = LoggerFactory.getLogger(UserController.class);
+        Logger LOG = LoggerFactory.getLogger(UserController.class);
+        User user1 = userService.login(phone, password);
+        if (user1.getId() == (-1)) {
+            LOG.info("user: "+phone+" does not exist, pwd: "+password);
+        }
+        else if (user1.getId() == (-2)) {
+            LOG.info("user: "+phone+" failed to login, wrong pwd: "+password);
+        }
+        else{
+            LOG.info("user: "+phone+" login successfully pwd: "+password);
+        }
+        return user1;
     }
 
     @PostMapping("/tokenLogin")
@@ -71,23 +116,40 @@ public class UserController {
 
         User user = JSON.toJavaObject(jsonObject, User.class);
 
+        Logger LOG = LoggerFactory.getLogger(UserController.class);
+        LOG.info(" token login : "+token);
         return login(user.getPhone(), user.getPassword());
     }
 
     @PostMapping("/update")
     public String update(@RequestBody RegisterUtil registerUtil) {
+        Logger LOG = LoggerFactory.getLogger(UserController.class);
+        LOG.info("user: "+registerUtil.getPhone()+" update message");
         return userService.update(registerUtil);
     }
 
     @PostMapping("/follow")
     public String follow(@RequestParam("sub") Integer sub, @RequestParam("obj") Integer obj,
                     @RequestParam("flag") Integer flag) {
+        Logger LOG = LoggerFactory.getLogger(UserController.class);
         if (Objects.equals(sub, obj))
+        {
+            LOG.info("user: "+sub+" followed himself");
             return "self";
+        }
+
 
         // 1 means follow and -1 means un follow
         if (flag == 1 || flag == -1) {
             userService.follow_relation(sub, obj, flag);
+            if(flag == 1)
+            {
+                LOG.info("user: "+sub+" followed "+obj);
+            }
+            else
+            {
+                LOG.info("user: "+sub+" unfollowed "+obj);
+            }
             return "success";
         }
 
@@ -98,12 +160,21 @@ public class UserController {
     @PostMapping("/auth")
     public String auth(@RequestParam("sub") Integer sub, @RequestParam("obj") Integer obj,
                        @RequestParam("tar") Integer target) {
+        Logger LOG = LoggerFactory.getLogger(UserController.class);
         if (target < -8 || target >= 8)
+        {
+            LOG.info("user: "+sub+" request wrong permission ");
             return "target error";
+        }
+
 
         if (Objects.equals(sub, obj))
+        {
+            LOG.info("user: "+sub+" request  permission from himself");
             return "self";
-
+        }
+//            return "self";
+        LOG.info("user: "+sub+" request "+target+" permission from user: "+obj );
         return userService.auth(sub, obj, target);
     }
 
